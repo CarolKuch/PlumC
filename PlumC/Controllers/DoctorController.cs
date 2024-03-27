d42f6a83e47e626de4c7dcb712e617804e180884
CarolKuch <kar.kucharska@gmail.com>
2024-03-27 11:09:18 +0100

    added actions in controller

--- PlumC/Controllers/DoctorController.cs
+++ PlumC/Controllers/DoctorController.cs
@@ -8,18 +8,17 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using PlumC.Models;
-using Microsoft.EntityFrameworkCore;
 
 namespace PlumC.Controllers
 {
     public class DoctorController : Controller
     {
-        private PlumContext _db = new PlumContext();
+        private PlumContext db = new PlumContext();
 
         // GET: Doctor
         public async Task<ActionResult> Index()
         {
-            return View(await _db.Doctors.ToListAsync());
+            return View(await db.Doctors.ToListAsync());
         }
 
         // GET: Doctor/Details/5
@@ -29,7 +28,7 @@ namespace PlumC.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Doctor doctor = await _db.Doctors.Where(x => x.Id == id).SingleOrDefaultAsync();
+            Doctor doctor = await FindDoctorByIdAsync(id);
             if (doctor == null)
             {
                 return HttpNotFound();
@@ -52,21 +51,83 @@ namespace PlumC.Controllers
         {
             if (ModelState.IsValid)
             {
-                _db.Doctors.Add(doctor);
-                await _db.SaveChangesAsync();
+                db.Doctors.Add(doctor);
+                await db.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
 
             return View(doctor);
         }
 
+        // GET: Doctor/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Doctor doctor = await FindDoctorByIdAsync(id);
+            if (doctor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(doctor);
+        }
+
+        // POST: Doctor/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Name,Surname,Major")] Doctor doctor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(doctor).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(doctor);
+        }
+
+        // GET: Doctor/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Doctor doctor = await FindDoctorByIdAsync(id);
+            if (doctor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(doctor);
+        }
+
+        // POST: Doctor/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Doctor doctor = await FindDoctorByIdAsync(id);
+            db.Doctors.Remove(doctor);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
         protected override void Dispose(bool disposing)
         {
             if (disposing)
             {
-                _db.Dispose();
+                db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+        private async Task<Doctor> FindDoctorByIdAsync(int? id)
+        {
+            return await db.Doctors.Where(d => d.Id == id).SingleOrDefaultAsync();
         }
     }
 }
